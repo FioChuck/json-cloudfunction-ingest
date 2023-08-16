efe8835ccfde9205314efa326c282380b6a23a31
FiocChuck <chasf@google.com>
2023-08-16 16:43:19 -0400

    update source code

--- src/main.py
+++ src/main.py
@@ -1,53 +1,25 @@
-import requests
-import os
-from google.cloud import pubsub_v1
-import json
+import functions_framework
 
+# Triggered by a change in a storage bucket
+@functions_framework.cloud_event
+def hello_gcs(cloud_event):
+    data = cloud_event.data
 
-# def fetch_message():
+    event_id = cloud_event["id"]
+    event_type = cloud_event["type"]
 
-#     url = "https://data.alpaca.markets/v2/stocks/GOOGL/snapshot"
+    bucket = data["bucket"]
+    name = data["name"]
+    metageneration = data["metageneration"]
+    timeCreated = data["timeCreated"]
+    updated = data["updated"]
 
-#     # https://data.alpaca.markets/v2/stocks/GOOGL/trades/latest
-#     # https://data.alpaca.markets/v2/stocks/GOOGL/snapshot
+    print(f"Event ID: {event_id}")
+    print(f"Event type: {event_type}")
+    print(f"Bucket: {bucket}")
+    print(f"File: {name}")
+    print(f"Metageneration: {metageneration}")
+    print(f"Created: {timeCreated}")
+    print(f"Updated: {updated}")
 
-#     key_id = os.getenv('KEY_ID')
-#     secret_key = os.getenv('SECRET_KEY')
-
-#     payload = {}
-#     headers = {
-#         'Apca-Api-Key-Id': key_id,
-#         'Apca-Api-Secret-Key': secret_key
-#     }
-
-#     response = requests.request("GET", url, headers=headers, data=payload)
-#     out = response.json()
-
-#     return out
-
-
-# def publish_message(data):
-#     project_id = os.getenv('PROJECT_ID')
-#     topic_id = os.getenv('TOPIC_ID')
-
-#     publisher = pubsub_v1.PublisherClient()  # instantiate client
-
-#     topic_path = publisher.topic_path(project_id, topic_id)
-
-#     message_json = json.dumps(data)
-#     message_bytes = message_json.encode('utf-8')
-
-#     future = publisher.publish(topic_path, message_bytes)
-#     print(future.result())
-
-#     return
-
-
-def main(request):
-    api_results = fetch_message()
-    publish_message(api_results)
-
-    return 'success'
-
-
-# main('')
+    print("gs://" + bucket + "/" + name)
